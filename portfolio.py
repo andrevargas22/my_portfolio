78271634ec3e3a98e4eff6af1bb2f29d039a5fd4
Andre Vargas <andrevargas22@gmail.com>
2024-04-24 13:46:40 -0300

    ajustes basicos

--- portfolio.py
+++ portfolio.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/sentiment_analysis')
 def sentiment_analysis():
     return render_template('sentiment_analysis.html')
