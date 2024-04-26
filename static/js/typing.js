37d87f2d83f0c8991b2fe45651ca904870d1191d
Andre Vargas <andrevargas22@gmail.com>
2024-04-26 20:10:55 -0300

    new index page, missing size adjustments

---
+++ static/js/typing.js
@@ -0,0 +1,41 @@
+const words = ["a Machine Learning Engineer", "a Data Scientist", "a dog lover", "a gamer", "a drummer"];
+
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			timer = setTimeout(deletingEffect, 1000);
+			return false;
+		};
+		timer = setTimeout(loopTyping, 50);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 25);
+        
+	};
+	loopDeleting();
+};
+
+typingEffect();
