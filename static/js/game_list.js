b8af4fb08216076865a5752b9805addc90dca3a4
Andre Vargas <andrevargas22@gmail.com>
2024-05-23 15:30:38 -0300

    games finished

--- static/js/game_list.js
+++ static/js/game_list.js
@@ -1,23 +1,30 @@
-$('.portfolio-item').isotope({
-    itemSelector: '.item',
-    layoutMode: 'fitRows'
-});
-$('.portfolio-menu ul li').click(function(){
-    $('.portfolio-menu ul li').removeClass('active');
-    $(this).addClass('active');
-    
-    var selector = $(this).attr('data-filter');
-    $('.portfolio-item').isotope({
-        filter:selector
-    });
-    return  false;
-});
 $(document).ready(function() {
-var popup_btn = $('.popup-btn');
-popup_btn.magnificPopup({
-type : 'image',
-gallery : {
-    enabled : true
-}
-});
+    // Initialize Magnific Popup
+    var popup_btn = $('.popup-btn');
+    popup_btn.magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    // Initialize Isotope after images have loaded
+    var $grid = $('.portfolio-item').imagesLoaded(function() {
+        $grid.isotope({
+            itemSelector: '.item',
+            layoutMode: 'fitRows'
+        });
+    });
+
+    // Filter items on button click
+    $('.portfolio-menu ul li').click(function() {
+        $('.portfolio-menu ul li').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: selector
+        });
+        return false;
+    });
 });
